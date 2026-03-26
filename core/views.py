ab00267983c50b4de1e50da89b3e556710a4602c
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-26 14:23:48 +0200

    removed double faces

--- core/views.py
+++ core/views.py
@@ -12,5 +12,8 @@ def home(request):
 
     face_1 = Face.objects.all()[random_index_1]
     face_2 = Face.objects.all()[random_index_2]
+    if face_1 == face_2:
+        random_index_2 = random.randint(0, count_2 - 1)
+        face_2 = Face.objects.all()[random_index_2]
 
     return render(request, 'home.html', { 'face_1': face_1, 'face_2': face_2 })
