871deef919d54fb6b2d79daab15813f90f492d5f
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-26 13:35:03 +0200

    added random selection for faces

--- core/views.py
+++ core/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+import random
+from .models import Face, Mash
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    count_1 = Face.objects.all().count()
+    count_2 = Face.objects.all().count()
+
+    random_index_1 = random.randint(0, count_1 - 1)
+    random_index_2 = random.randint(0, count_2 - 1)
+
+    face_1 = Face.objects.all()[random_index_1]
+    face_2 = Face.objects.all()[random_index_2]
+
+    return render(request, 'home.html', { 'face_1': face_1, 'face_2': face_2 })
