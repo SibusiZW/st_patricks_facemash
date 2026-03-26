7b66df2e6b7e49b5c3244bdd2414abe76ab784ef
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-26 14:56:52 +0200

    added onrender.com to ALLOWED_HOSTS

--- st_patricks_facemash/settings.py
+++ st_patricks_facemash/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*.onrender.com', 'localhost', '127.0.0.1']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
