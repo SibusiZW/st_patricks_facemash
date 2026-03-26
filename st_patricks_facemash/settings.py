5ff764220852fbca2402efd42e1836f03b0db366
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-26 12:26:13 +0200

    Added env variable for secret key

--- st_patricks_facemash/settings.py
+++ st_patricks_facemash/settings.py
@@ -12,7 +12,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*d!58@dnv*9rqt%m9imqj$c^3v8!7bl9)$7+s*7ody7z22p3t@'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
